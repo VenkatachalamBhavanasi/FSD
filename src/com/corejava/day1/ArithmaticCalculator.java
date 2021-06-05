1b7dbe264e66262905143a0ef000650376947b2d
venkatachalam.bhavan <venkatachalam.bhavan@servicenow.com>
2021-06-05 18:26:21 +0530

    first commit

---
+++ src/com/corejava/day1/ArithmaticCalculator.java
@@ -0,0 +1,33 @@
+package com.corejava.day1;
+
+public class ArithmaticCalculator {
+	/*
+	 * public static void main(String[] args) { // TODO Auto-generated method stub
+	 * 
+	 * System.out.println("Arithmatic Calculator"); int a =
+	 * Integer.parseInt(args[0]); int b = Integer.parseInt(args[1]);
+	 * 
+	 * System.out.println("Addition :" + (a + b)); System.out.println("Subraction :"
+	 * + (a - b)); System.out.println("Multiplication :" + a * b);
+	 * System.out.println("division :" + a / b);
+	 * 
+	 * }
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Arithmatic Calculator");
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+
+		System.out.println("Addition :" + (a + b));
+		System.out.println("Subraction :" + (a - b));
+		System.out.println("Multiplication :" + a * b);
+		System.out.println("division :" + a / b);
+		
+	
+
+	}
+
+}
